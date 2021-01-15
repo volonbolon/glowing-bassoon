81f125a3851910230ecbce372d041be43d557995
Ariel Rodriguez <ariel@volonbolon.me>
2021-01-15 19:47:28 -0300

    Initial Commit

---
+++ SwiftUINews/SwiftUINewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINewsApp.swift
+//  SwiftUINews
+//
+//  Created by Ariel Rodriguez on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
